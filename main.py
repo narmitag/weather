70130f3178117d14e5c1f3dacb28ceaab19540ec
neilarmitage <neil@neila.co.uk>
2021-02-14 12:24:32 +0000

    new version

--- main.py
+++ main.py
@@ -31,20 +31,61 @@ def process_data_file(filetype, day, month,year):
 
         mycursor = mydb.cursor()
 
-        sql = "REPLACE INTO all_observations (stationID, obsTimeLocal, obsTimeUtc) VALUES (%s, %s, %s)"
-        val = (ob['stationID'], ob['obsTimeLocal'], ob['obsTimeUtc'])
+        tablename = f'{filetype}_observations'
+        sql = f"REPLACE INTO {tablename}   " \
+              f"VALUES (%s, %s, %s,%s, %s, %s,%s, %s, %s, %s," \
+              f"%s, %s, %s,%s, %s, %s,%s, %s, %s, %s," \
+              f"%s, %s, %s,%s, %s, %s,%s, %s, %s, %s," \
+              f"%s, %s, %s,%s, %s, %s,%s)"
+        val = (ob['stationID'],
+                ob['tz'],
+                ob['obsTimeUtc'],
+                ob['obsTimeLocal'],
+                ob['epoch'],
+                ob['lat'],
+                ob['lon'],
+                ob['solarRadiationHigh'],
+                ob['uvHigh'],
+                ob['winddirAvg'],
+                ob['humidityHigh'],
+                ob['humidityLow'],
+                ob['humidityAvg'],
+                ob['qcStatus'],
+                ob['metric']['tempHigh'],
+                ob['metric']['tempLow'],
+                ob['metric']['tempAvg'],
+                ob['metric']['windspeedHigh'],
+                ob['metric']['windspeedLow'],
+                ob['metric']['windspeedAvg'],
+                ob['metric']['windgustHigh'],
+                ob['metric']['windgustLow'],
+                ob['metric']['windgustAvg'],
+                ob['metric']['dewptHigh'],
+                ob['metric']['dewptLow'],
+                ob['metric']['dewptAvg'],
+                ob['metric']['windchillHigh'],
+                ob['metric']['windchillLow'],
+                ob['metric']['windchillAvg'],
+                ob['metric']['heatindexHigh'],
+                ob['metric']['heatindexLow'],
+                ob['metric']['heatindexAvg'],
+                ob['metric']['pressureMax'],
+                ob['metric']['pressureMin'],
+                ob['metric']['pressureTrend'],
+                ob['metric']['precipRate'],
+                ob['metric']['precipTotal'])
+
+
         mycursor.execute(sql, val)
 
         mydb.commit()
-
-    exit()
 
 
 def load_data(day, month, year):
     print(f'... processing {day}')
     process_data_file('all', day, month, year)
-    #process_data_file('daily', day, month, year)
-    #process_data_file('hourly', day, month, year)
+    process_data_file('daily', day, month, year)
+    process_data_file('hourly', day, month, year)
 
 
 if __name__ == '__main__':
